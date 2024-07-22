2867c616d3d3a412edb8b5ef3bf19ee3e69576cc
djmorphy <53020220+djmorphy@users.noreply.github.com>
2024-07-22 14:11:07 +0200

    s1 feltoltese

---
+++ s1.ps1
@@ -0,0 +1,8 @@
+get-childItem 
+Get-ExecutionPolicy
+get-help Set-ExecutionPolicy -full
+Update-Help
+Update-Help -SourcePath
+Save-Help -DestinationPath
+get-help Set-ExecutionPolicy -ShowWindow
+get-help about*
